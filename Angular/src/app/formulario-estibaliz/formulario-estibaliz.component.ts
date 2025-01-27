cf8e774233f4caf719ce34c8e0008a9ac4d2f968
estibalizcortes <cortesgarciaestibaliz@gmail.com>
2025-01-27 12:49:38 +0100

    creo formulario en clase siguiendo apuntes

---
+++ Angular/src/app/formulario-estibaliz/formulario-estibaliz.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-estibaliz',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './formulario-estibaliz.component.html',
+  styleUrl: './formulario-estibaliz.component.css'
+})
+export class FormularioEstibalizComponent {
+  title= 'Formulario de Registro';
+  persona ={
+    nombre: '',
+    apellido: '',
+    correo: '',
+    contrasena: '',
+    confirmarContrasena: '',
+    direccion: '',
+    telefono: '',
+    fechaNacimiento: '',
+    genero: '',
+    estadoCivil: '' 
+  }
+}
