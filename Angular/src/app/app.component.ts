bd0b953e36578444c2ae8f0da9a759f5c6c97a8c
Fferalv <fferalv454@iesalmudeyne.es>
2025-01-27 05:03:34 +0100

    Añadido el main

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component'; 
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent], 
+  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent, MainComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
