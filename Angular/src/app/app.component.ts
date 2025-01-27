148f0b4cc433182ef84df06f374b382472f76da8
davgarvel <dagave05@gmail.com>
2025-01-27 01:08:31 +0100

    Prueba del css del footer

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule, HeaderComponent], 
+  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
