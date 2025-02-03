bf691f5bb64cc1d9a8439249c2137911964b5fb3
estibalizcortes <cortesgarciaestibaliz@gmail.com>
2025-02-03 13:05:16 +0100

    Revert "Merge branch 'estibaliz'"
    This reverts commit 786f99c727aeb90666d379a10661bbade88199de, reversing
    changes made to 209a1e471d7b7833b781f68df1344f0953a7ee07.

--- Angular/src/app/formulario-estibaliz/formulario-estibaliz.component.ts
+++ Angular/src/app/formulario-estibaliz/formulario-estibaliz.component.ts
@@ -9,39 +9,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './formulario-estibaliz.component.css'
 })
 export class FormularioEstibalizComponent {
-  validarCorreo() {
-    let correo = this.persona.correo;
-    if (correo.indexOf('@') === -1 || !correo.endsWith('.com') || !correo.endsWith('.es')) {
-      console.error('El correo no es válido, debe tener un @ y acabar en .com / .es');
-    }
-  }
-  validarDireccion() {
-    if (!this.persona.direccion) {
-      console.error('La dirección es obligatoria');
-    }
-  }
-  restaurarBoton() {
-    const boton = document.querySelector('button');
-    if (boton) {
-      boton.style.width = 'auto';
-      boton.style.height = 'auto';
-    }
-  }
-  agrandarBoton() {
-    const boton = document.getElementById("botonEnviar");
-    if (boton) {
-      boton.style.width = '200px';
-      boton.style.height = '100px';
-    }
-  }
-
-  reducirBoton() {
-    const boton = document.getElementById("botonEnviar");
-    if (boton) {
-      boton.style.width = 'auto';
-      boton.style.height = 'auto';
-    }
-  }
   title= 'Formulario de Registro';
   persona ={
     nombre: '',
