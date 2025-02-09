74f0f6aa6ed11ff9a989e4851b4f0f757f4f4412
fferalv <fferalv454@iesalmudeyne.es>
2025-02-09 21:50:11 +0100

    evento

--- Angular/src/app/formulario-paco/formulario-paco.component.ts
+++ Angular/src/app/formulario-paco/formulario-paco.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-formulario-paco',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],  
   templateUrl: './formulario-paco.component.html',
   styleUrls: ['./formulario-paco.component.css']
 })
 export class FormularioPacoComponent {
-  title= 'Formulario de Registro';
+  title = 'Formulario de Registro';
   color: string = 'black'; // Color inicial de las letras
 
   persona = {
@@ -27,14 +28,14 @@ export class FormularioPacoComponent {
 
   constructor() { }
 
-  // Cambiar el color a azul claro cuando el ratón pase por encima
+  // De color a azul claro cuando el ratón pase por encima
   changeColor(event: any): void {
-    this.color = '#ADD8E6'; // Azul clarito
+    this.color = '#ADD8E6'; 
   }
 
   // Volver al color negro cuando el ratón salga
   resetColor(event: any): void {
-    this.color = 'black'; // Volver al negro
+    this.color = 'black'; 
   }
 
   enviarDatos() {
